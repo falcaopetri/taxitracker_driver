70126cca79c61d6790a1476925b2e31d2180af46
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2017-01-17 20:11:12 -0200

    Calling createRace (como exemplo)

--- app/src/main/java/br/ufscar/dc/mds/taxitracker_driver/MainActivity.java
+++ app/src/main/java/br/ufscar/dc/mds/taxitracker_driver/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+
 import br.ufscar.dc.mds.taxitracker_library.TaxiTrackerRestClientUsage;
 import br.ufscar.dc.mds.taxitracker_library.TaxiTrackerRestHandler;
 
 public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHandler {
     TaxiTrackerRestClientUsage taxiTrackerRest;
+    private final static String AUTH_USER_TAG = "motorista";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,13 @@ public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHa
     }
 
     public void login(View view) {
+        // método chamando como callback de um botão
+
         // generate the token @ http://stackoverflow.com/a/27904796
         // TODO esse id_token é o retornado pela API do google Oauth 2.0 após a autenticação
         // Dica: É uma string bem longa...
         String id_token = "";
-        taxiTrackerRest.login(id_token);
+        taxiTrackerRest.login(id_token, AUTH_USER_TAG);
     }
 
     @Override
@@ -38,5 +43,11 @@ public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHa
         taxiTrackerRest.add_auth_token(access_token);
 
         taxiTrackerRest.getPassageiros();
+
+        try {
+            taxiTrackerRest.createRace(this, "origem", "destino");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
